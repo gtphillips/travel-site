0975abe0d2d191550f9830af964b12e2d92b75d9
Greg <greg-phillips@live.co.uk>
2018-08-05 15:39:44 +0100

    header animations completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,3 @@
-import $ from 'jquery';
-import Person from './modules/Person';
+import MobileMenu from "./modules/MobileMenu";
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " has now paid their taxes.");
-  }
-}
-
-alert("Winning");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
