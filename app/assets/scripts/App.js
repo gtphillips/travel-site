3a27098f9059ff0a8eccbed4eef4cd1c35421b8d
Greg <greg-phillips@live.co.uk>
2018-08-06 18:07:18 +0100

    updated waypoint value

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,7 @@ import StickyHeader from "./modules/StickyHeader";
 import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "45%");
+new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testamonial"), "60%");
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
